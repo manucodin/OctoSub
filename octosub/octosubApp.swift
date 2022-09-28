9aec3404a544dd61e20cbc49a6945d75ebe17f9a
Manu Rodriguez <manurs93@gmail.com>
2022-09-28 23:18:23 +0200

    First Commit

---
+++ octosub/octosubApp.swift
@@ -0,0 +1,17 @@
+//
+//  octosubApp.swift
+//  octosub
+//
+//  Created by Manuel Rodriguez Sebastian on 28/9/22.
+//
+
+import SwiftUI
+
+@main
+struct octosubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
